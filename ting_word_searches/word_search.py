bc8e2f6b13d6679504940a60f50cf7f12a12580a
caren <carenoliveirasilvapontes@gmail.com>
2023-06-06 16:56:42 -0300

    feat:exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in range(len(instance)):
+        news = instance.search(i)
+        info = {
+            "palavra": word,
+            "arquivo": news["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for j, line in enumerate(news["linhas_do_arquivo"], 1):
+            if word.lower() in line.lower():
+                info["ocorrencias"].append({"linha": j})
+        if info["ocorrencias"]:
+            result.append(info)
+    return result
 
 
 def search_by_word(word, instance):
