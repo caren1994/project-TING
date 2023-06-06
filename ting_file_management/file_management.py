40407f03cb0f8a8571cb13592ccf0543086c8101
caren <carenoliveirasilvapontes@gmail.com>
2023-06-06 14:46:00 -0300

    feat:implementado txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            sys.stderr.write("Formato inválido")
+
+        with open(path_file) as file:
+            content = file.read().split("\n")
+            return content
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
