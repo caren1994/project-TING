5d24524a77eeea5b8358773045504148ce8f10cd
caren <carenoliveirasilvapontes@gmail.com>
2023-06-06 15:18:08 -0300

    feat:process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in range(len(instance)):
+        if instance.search(item)["nome_do_arquivo"] == path_file:
+            return False
+
+    content = txt_importer(path_file)
+
+    info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content,
+    }
+    instance.enqueue(info)
+    sys.stdout.write(str(info))
 
 
 def remove(instance):
